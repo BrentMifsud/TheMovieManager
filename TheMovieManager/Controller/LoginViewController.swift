26d2fcd99aafd4d9b31883c27d74a0add32aae7d
Brent Mifsud <brentmifsud@icloud.com>
2019-05-31 19:48:06 -0400

    disable login ui elements when login is in process

--- TheMovieManager/Controller/LoginViewController.swift
+++ TheMovieManager/Controller/LoginViewController.swift
@@ -72,6 +72,10 @@ class LoginViewController: UIViewController {
 		} else {
 			activityIndicator.stopAnimating()
 		}
+		emailTextField.isEnabled = !loggingIn
+		passwordTextField.isEnabled = !loggingIn
+		loginButton.isEnabled = !loggingIn
+		loginViaWebsiteButton.isEnabled = !loggingIn
 	}
 	
 }
